d615638c0c6042677bcaf7faad5292a4e07382da
Stephan timmer <stephan@liquidstacks.nl>
2020-02-09 20:28:24 +0100

    rough responsive content for mobile and tablet added/ change of file structure.
    a rough setup has been added for mobile and tablet viewport. media queries created. / changed project structure. i've made it so that every page has its own folder with their own respective content within it.

---
+++ views/home/index.php
@@ -0,0 +1,22 @@
+<?php include('../partials/header.php'); ?>
+
+<div class="container">
+    <div class="row">
+        <section class="col-12  col-lg-6 mb-3">
+            <img class="home-big-block" src=images/fietsen_cr.jpg alt="">
+            <h1 class="title-fietsen text-center"><strong>Fietsen</strong></h1>
+        </section>
+
+        <section class="col-12 col-lg-6  mb-2">
+            <img  class="home-medium-block" src=images/reparatie_ncr.jpg alt="">
+            <h1 class="title-reparatie text-center"><strong>Reparatie</strong></h1>
+        </section>
+
+        <section class="col-12 col-lg-6  mt-2 mb-5">
+            <img  class="home-medium-block" src=images/opleiding_ncr.jpg alt="">
+            <h1 class="title-opleiding text-center"><strong>Opleiding</strong></h1>
+        </section>
+    </div>
+</div>
+
+<?php include('../partials/footer.php'); ?>
--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,5 @@
 </body>
 <script src="https://kit.fontawesome.com/24c2525181.js" crossorigin="anonymous"></script>
+<script src="../assets/js/imageScroll.js"></script>
+
 </html>
